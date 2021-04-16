e465b267b9b266c698f2f87ca3aa9dd78940fb8a
Eddy Ekofo <eddyecko@gmail.com>
2021-04-16 01:40:24 +0100

    Change on_attach to a global one

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -4,14 +4,5 @@ require'lspconfig'.clangd.setup {
             "--suggest-missing-includes",
             "--clang-tidy",
             "--header-insertion=iwyu"},
-    on_attach = require'lsp'.common_on_attach,
-    handlers = {
-        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-            virtual_text = O.clang.diagnostics.virtual_text,
-            signs = O.clang.diagnostics.signs,
-            underline = O.clang.diagnostics.underline,
-            update_in_insert = true
-
-        })
-    }
+    on_attach = Custom_attach
 }
