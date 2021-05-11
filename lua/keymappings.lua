89c9b33bfb8e784533fd0b39a9c94b3067aa4a41
Eddy Ekofo <eddyecko@gmail.com>
2021-05-11 10:51:21 +0100

    Keymaps clean-up

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -3,17 +3,7 @@ local mapper = function(mode, key, result)
     vim.api.nvim_set_keymap(mode, key, result, {noremap = true, silent = true})
 end
 
--- Telescope integration
--- mapper("n", "<Leader>ff", "<cmd>lua require'telescope.builtin'.find_files{}<CR>") -- search all files, respecting .gitignore if one exists
--- mapper("n", "<Leader>fb", "<cmd>lua require'telescope.builtin'.buffers{}<CR>") -- search open buffers
--- mapper("n", "<Leader>fl", "<cmd>lua require'telescope.builtin'.treesitter{}<CR>") -- search symbols in current buffer
--- mapper("n", "<Leader>gg", "<cmd>lua require'telescope.builtin'.live_grep{}<CR>") -- search all lines in project
--- mapper("n", "<Leader>fr", "<cmd>lua require'telescope.builtin'.lsp_references{}<CR>") -- search references to symbol under cursor
--- mapper("n", "<Leader>co", "<cmd>lua require'telescope.builtin'.colorscheme{}<CR>") -- Fuzzy find colorschemes
--- mapper("n", "<Leader>cm", "<cmd>lua require'telescope.builtin'.commands{}<CR>") -- command history
-mapper("n", "q", ":wq<CR>") -- force write before quit TODO: see if it's a good idea or not
-
-     vim.api.nvim_set_keymap("n", "<Space>", "<NOP>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Space>", "<NOP>", {noremap = true, silent = true})
 vim.g.mapleader = " "
 vim.cmd("let g:UltiSnipsExpandTrigger = '<F5>'")
 
