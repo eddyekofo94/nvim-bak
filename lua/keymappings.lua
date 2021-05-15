01c06e189d3111a7ea023b151ccde67cb36aadec
Eddy Ekofo <eddyecko@gmail.com>
2021-05-15 02:42:10 +0100

    Clean up

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -29,8 +29,8 @@ vim.api.nvim_set_keymap("i", "kj", "<ESC>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("i", "jj", "<ESC>", {noremap = true, silent = true})
 
 -- Tab switch buffer
-vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", {noremap = true, silent = true})
 
 -- Move selected line / block of text in visual mode
 vim.api.nvim_set_keymap("x", "K", ":move '<-2<CR>gv-gv", {noremap = true, silent = true})
