b95233a2fdf81769aa813642175a6b4d649f3c9d
Eddy Ekofo <eddyecko@gmail.com>
2021-05-17 22:41:49 +0100

    trying to get the snippets to work

--- lua/lsp/compe.lua
+++ lua/lsp/compe.lua
@@ -3,13 +3,13 @@ vim.o.completeopt = "menuone,noselect"
 require "compe".setup {
     enabled = true,
     autocomplete = true,
-    debug = false,
+    debug = true, -- BUG: enabled for debugging.. to be removed
     min_length = 1,
     preselect = "enable",
     throttle_time = 80,
     source_timeout = 200,
     incomplete_delay = 400,
-    allow_prefix_unmatch = false,
+    -- allow_prefix_unmatch = false,
     max_abbr_width = 100,
     max_kind_width = 100,
     max_menu_width = 100,
@@ -21,7 +21,7 @@ require "compe".setup {
         spell = {kind = "  "},
         path = {kind = "  "},
         calc = {kind = "  "},
-        -- vsnip = {kind = "  "}, -- BUG: not working... fix
+        vsnip = {kind = "  "}, -- BUG: not working... fix
         nvim_lua = true,
         tags = false,
         vim_dadbod_completion = true,
