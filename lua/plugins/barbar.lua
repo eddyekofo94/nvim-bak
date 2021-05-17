c7b257772e229872b0ca5d41bb460e5eb7e9b5e8
Eddy Ekofo <eddyecko@gmail.com>
2021-05-17 22:41:02 +0100

    barbar & nvim-tree enhancements

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -7,3 +7,16 @@ vim.cmd "hi default link BufferCurrentSign  Normal"
 
 -- For buffers visible but not the current one
 vim.cmd "hi default link BufferVisible TabLineSel"
+
+local tree ={}
+tree.open = function ()
+   require'bufferline.state'.set_offset(31)
+   require'nvim-tree'.find_file(true)
+end
+
+tree.close = function ()
+   require'bufferline.state'.set_offset(0)
+   require'nvim-tree'.close()
+end
+
+return tree
