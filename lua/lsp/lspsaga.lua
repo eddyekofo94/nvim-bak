f1ba4be130904ff97a8bff453d13043551eb26dd
Eddy Ekofo <eddyecko@gmail.com>
2021-05-17 22:39:26 +0100

    Add info sign in lsp

--- lua/lsp/lspsaga.lua
+++ lua/lsp/lspsaga.lua
@@ -21,6 +21,7 @@ local opts = {
     error_sign = "",
     warn_sign = "",
     hint_sign = "",
+    infor_sign = "",
     code_action_icon = " ",
     rename_prompt_prefix = ">"
 }
@@ -32,10 +33,8 @@ mapper("n", "H", "<cmd>lua require'lspsaga.codeaction'.code_action()<CR>") --
 mapper("n", "gs", "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>")
 mapper("n", "gr", "<cmd>lua require('lspsaga.rename').rename()<CR>")
 mapper("n", "gd", "<cmd>lua require'lspsaga.provider'.preview_definition()<CR>")
--- FIX: since it os not working :/
 mapper("n", "gh", "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>") -- search all files, respecting .gitignore if one exists
 
--- FIX: why it's not working?
 mapper("n", "<c-f>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>")
 mapper("n", "<c-b>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>")
 
