418e083229a8b9fb6fbd5b8de0ef4a1a4892fa0d
Eddy Ekofo <eddyecko@gmail.com>
2021-05-26 00:22:08 +0100

    Clean up

--- lua/eekofo/plugins/barbar.lua
+++ lua/eekofo/plugins/barbar.lua
@@ -1,6 +1,9 @@
-vim.api.nvim_set_keymap("n", "<TAB>", ":BufferNext<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<S-TAB>", ":BufferPrevious<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<S-x>", ":BufferClose<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<TAB>", ":BufferNext<CR>",
+                        {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<S-TAB>", ":BufferPrevious<CR>",
+                        {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<S-x>", ":BufferClose<CR>",
+                        {noremap = true, silent = true})
 
 -- For the current active buffer icon
 vim.cmd "hi default link BufferCurrentSign  Normal"
@@ -8,15 +11,3 @@ vim.cmd "hi default link BufferCurrentSign  Normal"
 -- For buffers visible but not the current one
 vim.cmd "hi default link BufferVisible TabLineSel"
 
-local tree ={}
-tree.open = function ()
-   require'bufferline.state'.set_offset(31)
-   require'nvim-tree'.find_file(true)
-end
-
-tree.close = function ()
-   require'bufferline.state'.set_offset(0)
-   require'nvim-tree'.close()
-end
-
-return tree
