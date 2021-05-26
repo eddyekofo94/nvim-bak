b4a4d3f5d4b3db23aa9e3dd2c71a7ac6b78f1068
Eddy Ekofo <eddyecko@gmail.com>
2021-05-26 01:06:13 +0100

    Enhancements

--- lua/eekofo/plugins/nvimtree.lua
+++ lua/eekofo/plugins/nvimtree.lua
@@ -1,11 +1,12 @@
 vim.g.nvim_tree_disable_netrw = 0 -- "1 by default, disables netrw
 -- vim.g.nvim_tree_hijack_netrw = 0 --"1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
-vim.g.nvim_tree_hide_dotfiles = 1 -- 0 by default, this option hides files and folders starting with a dot `.`
+vim.g.nvim_tree_hide_dotfiles = 0 -- 0 by default, this option hides files and folders starting with a dot `.`
 vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_follow = 1 -- "0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_close = O.auto_close_tree -- 0 by default, closes the tree when it's the last window
 vim.g.nvim_tree_auto_ignore_ft = {"dashboard", "startify"} --empty by default, don't auto open tree on specific filetypes.
 local tree_cb = require "nvim-tree.config".nvim_tree_callback
+
 vim.g.nvim_tree_bindings = {
     -- default mappings
     ["<CR>"] = tree_cb("edit"),
@@ -38,6 +39,7 @@ vim.g.nvim_tree_bindings = {
     ["-"] = tree_cb("dir_up"),
     ["q"] = tree_cb("close")
 }
+
 vim.g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1}
 vim.g.nvim_tree_icons = {
     default = "",
@@ -58,3 +60,16 @@ vim.g.nvim_tree_icons = {
         symlink = ""
     }
 }
+
+-- This apparently adjusts the nvimtree with barbar
+local tree = {}
+tree.open = function()
+    require'bufferline.state'.set_offset(31)
+    require'nvim-tree'.find_file(true)
+end
+
+tree.close = function()
+    require'bufferline.state'.set_offset(0)
+    require'nvim-tree'.close()
+end
+return tree
