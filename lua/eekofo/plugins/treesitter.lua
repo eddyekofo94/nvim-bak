21cfcd421328d8e60e9cbd5c47d82c7bd80ff569
Eddy Ekofo <eddyecko@gmail.com>
2021-06-10 02:24:10 +0100

    Format

--- lua/eekofo/plugins/treesitter.lua
+++ lua/eekofo/plugins/treesitter.lua
@@ -1,12 +1,10 @@
-require "nvim-treesitter.configs".setup {
+require"nvim-treesitter.configs".setup {
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
- ignore_install = { "haskell" },
+    ignore_install = {"haskell"},
     highlight = {
         enable = true -- false will disable the whole extension
     },
-    indent = {
-        enable = true
-    },
+    indent = {enable = true},
     refactor = {
         highlight_definitions = {enable = true},
         highlight_current_scope = {enable = false},
