a698e05ab8faa1dca387a5cf37f5e4d8fdb9c105
Eddy Ekofo <eddyecko@gmail.com>
2021-06-10 01:46:22 +0100

    Clean up

--- lua/eekofo/plugins/init.lua
+++ lua/eekofo/plugins/init.lua
@@ -1,39 +1,7 @@
 -- Various plugins which I installed
-
--- Have Neoformat only msg when there is an error
-vim.g.neoformat_only_msg_on_error = 1
-
--- Make Ranger to be hidden after picking a file
-vim.g.rnvimr_enable_picker = 1
-
--- TODO: move this to it's own file
-vim.g.indent_blankline_char = "▏"
-vim.g.indent_blankline_show_first_indent_level = true
-vim.g.indent_blankline_filetype_exclude = {
-    "startify", "dashboard", "dotooagenda", "log", "fugitive", "gitcommit",
-    "packer", "vimwiki", "markdown", "json", "txt", "vista", "help", "todoist",
-    "peekaboo", "git", "TelescopePrompt", "undotree", "flutterToolsOutline", ""
-}
-
-vim.g.indent_start_level = 1
-
-vim.g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_context_patterns = {
-    "class", "function", "method", "block", "list_literal", "selector", "^if",
-    "^table", "if_statement", "while", "for"
-}
-vim.cmd("autocmd CursorMoved * IndentBlanklineRefresh")
-
--- nvim-comment
-require("nvim_comment").setup()
-
 -- Initialize plugins
-require"colorizer".setup()
-require"surround".setup {}
-require"todo-comments".setup {}
 require("eekofo.plugins.telescope")
+require("eekofo.plugins.sessions")
 require("eekofo.plugins.dashboard")
 require("eekofo.plugins.web-devicons")
 require("eekofo.plugins.autopairs")
@@ -45,3 +13,18 @@ require("eekofo.plugins.treesitter")
 require("eekofo.plugins.which-key")
 require("eekofo.plugins.dap")
 require("eekofo.plugins.fterm")
+require("eekofo.plugins.indent_blankline")
+require"colorizer".setup()
+require("FTerm").setup()
+require"surround".setup {}
+require"todo-comments".setup {}
+
+-- nvim-comment
+require("nvim_comment").setup()
+
+-- Have Neoformat only msg when there is an error
+vim.g.neoformat_only_msg_on_error = 1
+
+-- Make Ranger to be hidden after picking a file
+vim.g.rnvimr_enable_picker = 1
+
