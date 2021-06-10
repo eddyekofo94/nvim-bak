5de64ea96139d5e60fcfa4688d272b72b69995db
Eddy Ekofo <eddyecko@gmail.com>
2021-06-10 02:22:37 +0100

    Clean up

--- lua/eekofo/plugins/nvimtree.lua
+++ lua/eekofo/plugins/nvimtree.lua
@@ -61,15 +61,3 @@ vim.g.nvim_tree_icons = {
     }
 }
 
--- This apparently adjusts the nvimtree with barbar
-local tree = {}
-tree.open = function()
-    require'bufferline.state'.set_offset(31)
-    require'nvim-tree'.find_file(true)
-end
-
-tree.close = function()
-    require'bufferline.state'.set_offset(0)
-    require'nvim-tree'.close()
-end
-return tree
