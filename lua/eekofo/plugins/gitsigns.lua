1ba635453a4a353da290af9fd407f5399cc10628
Eddy Ekofo <eddyecko@gmail.com>
2021-06-11 01:16:02 +0100

    Ran formatter

--- lua/eekofo/plugins/gitsigns.lua
+++ lua/eekofo/plugins/gitsigns.lua
@@ -1,11 +1,36 @@
 require("gitsigns").setup {
     signs = {
         -- TODO add hl to colorscheme..
-        add = {hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn"},
-        change = {hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn"},
-        delete = {hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn"},
-        topdelete = {hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn"},
-        changedelete = {hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn"}
+        add = {
+            hl = "GitSignsAdd",
+            text = "▎",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn"
+        },
+        change = {
+            hl = "GitSignsChange",
+            text = "▎",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
+        },
+        delete = {
+            hl = "GitSignsDelete",
+            text = "契",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
+        },
+        topdelete = {
+            hl = "GitSignsDelete",
+            text = "契",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
+        },
+        changedelete = {
+            hl = "GitSignsChange",
+            text = "▎",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
+        }
     },
     numhl = false,
     linehl = false,
@@ -13,12 +38,16 @@ require("gitsigns").setup {
         -- Default keymap options
         noremap = true,
         buffer = true,
-        ["n ]c"] = {expr = true, '&diff ? \']c\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\''},
-        ["n [c"] = {expr = true, '&diff ? \'[c\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\''}
+        ["n ]c"] = {
+            expr = true,
+            '&diff ? \']c\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\''
+        },
+        ["n [c"] = {
+            expr = true,
+            '&diff ? \'[c\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\''
+        }
     },
-    watch_index = {
-        interval = 1000
-    },
+    watch_index = {interval = 1000},
     sign_priority = 6,
     update_debounce = 200,
     status_formatter = nil, -- Use default
