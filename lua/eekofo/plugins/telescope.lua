a3c9f724db19e70f2a4901984fa965352aff4b47
Eddy Ekofo <eddyecko@gmail.com>
2021-06-13 16:02:00 +0100

    Clean up

--- lua/eekofo/plugins/telescope.lua
+++ lua/eekofo/plugins/telescope.lua
@@ -90,6 +90,5 @@ telescope.setup {
 require("telescope").load_extension("fzf")
 require "telescope".load_extension("project")
 require"telescope".load_extension("frecency")
---TODO Fix this, it is broken
 --require("telescope").extensions.fzf_writer.staged_grep() -- Added to which-key
 --require('telescope').extensions.fzf_writer.files()
