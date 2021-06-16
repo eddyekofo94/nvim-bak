0e198d1c621a9b94781f4ec5bc82754eb6c694c3
Eddy Ekofo <eddyecko@gmail.com>
2021-06-16 10:01:52 +0100

    Clean up

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 -- Initialise eveything from here
 
-vim.cmd("source ~/.config/nvim/vimscript/keys.vim")
 vim.cmd("luafile ~/.config/nvim/globals.lua")
 vim.cmd("luafile ~/.config/nvim/base-settings.lua")
 require("eekofo")
-require('plenary.reload').reload_module('eekofo', true)
