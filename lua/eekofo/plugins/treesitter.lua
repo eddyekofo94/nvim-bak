faad72963ced7869e916e7ff6de62ec90bbb92f9
Eddy Ekofo <eddyecko@gmail.com>
2021-06-19 23:41:23 +0100

    Add comment strings on tressitter

--- lua/eekofo/plugins/treesitter.lua
+++ lua/eekofo/plugins/treesitter.lua
@@ -2,9 +2,14 @@ require"nvim-treesitter.configs".setup {
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     ignore_install = {"haskell"},
     highlight = {
-        enable = true -- false will disable the whole extension
+        enable = true, -- false will disable the whole extension
+        use_languagetree = true
     },
     indent = {enable = true},
+    context_commentstring = {
+        enable = true,
+        config = {c = "// %s", lua = "-- %s", vim = "\" %s"}
+    },
     refactor = {
         highlight_definitions = {enable = true},
         highlight_current_scope = {enable = false},
