5b69f658db805e49d75c9968b983ee60cd64e50e
Eddy Ekofo <eddyecko@gmail.com>
2021-06-25 18:41:06 +0100

    Change the init file

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 -- Initialise eveything from here
 
+vim.cmd("luafile ~/.config/nvim/lua/eekofo/globals/init.lua")
 vim.cmd("luafile ~/.config/nvim/globals.lua")
 vim.cmd("luafile ~/.config/nvim/base-settings.lua")
+require("eekofo.globals")
 require("eekofo")
