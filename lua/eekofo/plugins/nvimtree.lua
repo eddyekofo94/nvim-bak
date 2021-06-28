09b8d9124c3f6ad157fb38bb15a65639cb8f4f23
Eddy Ekofo <eddyecko@gmail.com>
2021-06-28 11:44:33 +0100

    Enable ignore files to explorer

--- lua/eekofo/plugins/nvimtree.lua
+++ lua/eekofo/plugins/nvimtree.lua
@@ -1,6 +1,6 @@
 vim.g.nvim_tree_disable_netrw = 0 -- "1 by default, disables netrw
 -- vim.g.nvim_tree_hijack_netrw = 0 --"1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
-vim.g.nvim_tree_hide_dotfiles = 0 -- 0 by default, this option hides files and folders starting with a dot `.`
+vim.g.nvim_tree_hide_dotfiles = 1 -- 0 by default, this option hides files and folders starting with a dot `.`
 vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_follow = 1 -- "0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_close = O.auto_close_tree -- 0 by default, closes the tree when it's the last window
