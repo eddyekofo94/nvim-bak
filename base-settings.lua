db459e3146c5858cd1ad9a35a9aa8912bb778335
Eddy Ekofo <eddyecko@gmail.com>
2021-06-28 11:59:14 +0100

    Ran formatter

--- base-settings.lua
+++ base-settings.lua
@@ -10,15 +10,14 @@ an executable
 -- general
 O.auto_complete = true
 --O.colorscheme = 'gruvbox-material'
-O.colorscheme = 'gruvbox-flat'
+O.colorscheme = "gruvbox-flat"
 O.auto_close_tree = 1
 O.wrap_lines = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 O.treesitter.ensure_installed = "maintained"
-O.treesitter.ignore_install = {"haskell"}
+O.treesitter.ignore_install = { "haskell" }
 O.treesitter.highlight.enabled = true
-
 
 O.clang.diagnostics.virtual_text = false
 O.clang.diagnostics.signs = false
@@ -28,7 +27,7 @@ O.clang.diagnostics.underline = false
 -- add things like O.python.formatter.yapf.exec_path
 -- add things like O.python.linter.flake8.exec_path
 -- add things like O.python.formatter.isort.exec_path
-O.python.formatter = 'yapf'
+O.python.formatter = "yapf"
 -- O.python.linter = 'flake8'
 O.python.isort = true
 O.python.autoformat = true
@@ -38,11 +37,11 @@ O.python.diagnostics.underline = true
 
 -- lua
 -- TODO look into stylua
-O.lua.formatter = 'lua-format'
+O.lua.formatter = "lua-format"
 O.lua.autoformat = false
 
 -- javascript
-O.tsserver.formatter = 'prettier'
+O.tsserver.formatter = "prettier"
 O.tsserver.linter = nil
 O.tsserver.autoformat = true
 
