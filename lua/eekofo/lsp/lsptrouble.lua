72a859637d982657f76fdc0c2bce52aae94481ec
Eddy Ekofo <eddyecko@gmail.com>
2021-07-03 13:06:11 +0100

    Formatter

--- lua/eekofo/lsp/lsptrouble.lua
+++ lua/eekofo/lsp/lsptrouble.lua
@@ -1,22 +1,22 @@
 -- mapped to <space>lt -- this shows a list of diagnostics
 require("trouble").setup({
-  height = 15, -- height of the trouble list
-  mode = "lsp_document_diagnostics",
-  action_keys = {
-    -- key mappings for actions in the trouble list
-    close = "q", -- close the list
-    cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-    refresh = "r", -- manually refresh
-    jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
-    jump_close = { "o" }, -- jump to the diagnostic and close the list
-    toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
-    toggle_preview = "P", -- toggle auto_preview
-    hover = "K", -- opens a small poup with the full multiline message
-    preview = "p", -- preview the diagnostic location
-    close_folds = { "zM", "zm" }, -- close all folds
-    open_folds = { "zR", "zr" }, -- open all folds
-    toggle_fold = { "zA", "za" }, -- toggle fold of current file
-    previous = "k", -- preview item
-    next = "j", -- next item
-  },
+    height = 15, -- height of the trouble list
+    mode = "lsp_document_diagnostics",
+    action_keys = {
+        -- key mappings for actions in the trouble list
+        close = "q", -- close the list
+        cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+        refresh = "r", -- manually refresh
+        jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
+        jump_close = { "o" }, -- jump to the diagnostic and close the list
+        toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+        toggle_preview = "P", -- toggle auto_preview
+        hover = "K", -- opens a small poup with the full multiline message
+        preview = "p", -- preview the diagnostic location
+        close_folds = { "zM", "zm" }, -- close all folds
+        open_folds = { "zR", "zr" }, -- open all folds
+        toggle_fold = { "zA", "za" }, -- toggle fold of current file
+        previous = "k", -- preview item
+        next = "j", -- next item
+    },
 })
--- lua/eekofo/plugins/feline.lua
+++ lua/eekofo/plugins/feline.lua
@@ -2,61 +2,61 @@ local gruvbox_colors = O.gruvbox_colors
 local vi_mode = require("feline.providers.vi_mode")
 
 local conditions = {
-  buffer_not_empty = function()
-    return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
-  end,
-  hide_in_width = function()
-    return vim.fn.winwidth(0) > 80
-  end,
-  check_git_workspace = function()
-    local filepath = vim.fn.expand("%:p:h")
-    local gitdir = vim.fn.finddir(".git", filepath .. ";")
-    return gitdir and #gitdir > 0 and #gitdir < #filepath
-  end,
-  get_git_diff = function()
-    return vim.b.gitsigns_status_dict ~= nil
-  end,
-  -- vim.b.gitsigns_status_dict["added"]
+    buffer_not_empty = function()
+        return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+    end,
+    hide_in_width = function()
+        return vim.fn.winwidth(0) > 80
+    end,
+    check_git_workspace = function()
+        local filepath = vim.fn.expand("%:p:h")
+        local gitdir = vim.fn.finddir(".git", filepath .. ";")
+        return gitdir and #gitdir > 0 and #gitdir < #filepath
+    end,
+    get_git_diff = function()
+        return vim.b.gitsigns_status_dict ~= nil
+    end,
+    -- vim.b.gitsigns_status_dict["added"]
 }
 
 local icons = {
-  linux = " ",
-  macos = " ",
-  windows = " ",
+    linux = " ",
+    macos = " ",
+    windows = " ",
 }
 
 local function file_osinfo()
-  local os = vim.loop.os_uname().sysname
-  local icon
-  if os == "Linux" then
-    icon = icons.linux
-  elseif os == "Darwin" then
-    icon = icons.macos
-  else
-    icon = icons.windows
-  end
-  return icon
+    local os = vim.loop.os_uname().sysname
+    local icon
+    if os == "Linux" then
+        icon = icons.linux
+    elseif os == "Darwin" then
+        icon = icons.macos
+    else
+        icon = icons.windows
+    end
+    return icon
 end
 
 local colors = {
-  bg = gruvbox_colors.bg2,
-  black = gruvbox_colors.bg2,
-  yellow = "#d8a657",
-  cyan = "#89b482",
-  oceanblue = "#45707a",
-  green = "#a9b665",
-  orange = "#e78a4e",
-  violet = "#d3869b",
-  magenta = "#c14a4a",
-  white = "#d4be98",
-  fg = "#d4be98",
-  skyblue = "#7daea3",
-  red = "#ea6962",
+    bg = gruvbox_colors.bg2,
+    black = gruvbox_colors.bg2,
+    yellow = "#d8a657",
+    cyan = "#89b482",
+    oceanblue = "#45707a",
+    green = "#a9b665",
+    orange = "#e78a4e",
+    violet = "#d3869b",
+    magenta = "#c14a4a",
+    white = "#d4be98",
+    fg = "#d4be98",
+    skyblue = "#7daea3",
+    red = "#ea6962",
 }
 
 local get_diag = function(str)
-  local count = vim.lsp.diagnostic.get_count(0, str)
-  return (count > 0) and " " .. count .. " " or ""
+    local count = vim.lsp.diagnostic.get_count(0, str)
+    return (count > 0) and " " .. count .. " " or ""
 end
 
 -- local get_diff = function(str)
@@ -65,189 +65,181 @@ end
 -- end
 
 local vi_mode_provider = function()
-  local mode_alias = {
-    n = "NORMAL",
-    no = "NORMAL",
-    i = "INSERT",
-    v = "VISUAL",
-    V = "V-LINE",
-    [""] = "V-BLOCK",
-    c = "COMMAND",
-    cv = "COMMAND",
-    ce = "COMMAND",
-    R = "REPLACE",
-    Rv = "REPLACE",
-    s = "SELECT",
-    S = "SELECT",
-    [""] = "SELECT",
-    t = "TERMINAL",
-  }
-  -- return " " .. mode_alias[vim.fn.mode()] .. " "
-  return "     "
+    local mode_alias = {
+        n = "NORMAL",
+        no = "NORMAL",
+        i = "INSERT",
+        v = "VISUAL",
+        V = "V-LINE",
+        [""] = "V-BLOCK",
+        c = "COMMAND",
+        cv = "COMMAND",
+        ce = "COMMAND",
+        R = "REPLACE",
+        Rv = "REPLACE",
+        s = "SELECT",
+        S = "SELECT",
+        [""] = "SELECT",
+        t = "TERMINAL",
+    }
+    -- return " " .. mode_alias[vim.fn.mode()] .. " "
+    return "     "
 end
 
 local percentage_provider = function()
-  local cursor = require("feline.providers.cursor")
-  return " " .. cursor.line_percentage() .. " "
+    local cursor = require("feline.providers.cursor")
+    return " " .. cursor.line_percentage() .. " "
 end
 
 local vi_mode_hl = function()
-  return {
-    name = vi_mode.get_mode_highlight_name(),
-    fg = "bg",
-    bg = vi_mode.get_mode_color(),
-    style = "bold",
-  }
+    return {
+        name = vi_mode.get_mode_highlight_name(),
+        fg = "bg",
+        bg = vi_mode.get_mode_color(),
+        style = "bold",
+    }
 end
 
 require("feline").setup({
-  default_fg = gruvbox_colors.fg,
-  default_bg = gruvbox_colors.bg4,
-  colors = colors,
-  vi_mode_colors = {
-    NORMAL = "cyan",
-    OP = "cyan",
-    INSERT = "white",
-    VISUAL = "yellow",
-    BLOCK = "green",
-    REPLACE = "yellow",
-    ["V-REPLACE"] = "yellow",
-    ENTER = "cyan",
-    MORE = "cyan",
-    SELECT = "magenta",
-    COMMAND = "orange",
-    SHELL = "skyblue",
-    TERM = "skyblue",
-    NONE = "orange",
-  },
-  components = {
-    left = {
-      active = {
-        {
-          provider = vi_mode_provider,
-          hl = vi_mode_hl,
-        },
-        -- { provider = "", hl = vi_mode_hl },
-        {
-          provider = "file_info",
-          left_sep = " ",
-          right_sep = { str = "", hl = { fg = "black", bg = gruvbox_colors.bg4 } },
-        },
-        {
-          provider = "position",
-          hl = { fg = "violet", bg = "black" },
-        },
-        { provider = "", hl = { fg = "black", bg = "bg" } },
-        {
-          provider = "git_branch",
-          icon = "  ",
-          right_sep = " ",
-          enabled = conditions.get_git_diff,
-        },
-        {
-          -- provider = function()
-          -- 	return get_diff("added")
-          -- end,
-          hl = { fg = "bg", bg = "green", style = "bold" },
-          left_sep = { str = " ", hl = { fg = "green", bg = gruvbox_colors.bg4 } },
-          right_sep = { str = "", hl = { fg = "yellow", bg = "red" } },
-        },
-        -- { provider = "git_diff_added", hl = { fg = "green" } },
-        { provider = "git_diff_changed", hl = { fg = "yellow" } },
-        { provider = "git_diff_removed", hl = { fg = "red" } },
-        { provider = "", hl = { fg = "bg", bg = "black" } },
-      },
-      inactive = {
-        { provider = vi_mode_provider, hl = vi_mode_hl, right_sep = " " },
-        {
-          provider = "git_branch",
-          icon = " ",
-          right_sep = "  ",
-          enabled = function()
-            return vim.b.gitsigns_status_dict ~= nil
-          end,
-        },
-        { provider = "file_info" },
-        { provider = "", hl = { fg = "bg", bg = "black" } },
-      },
+    default_fg = gruvbox_colors.fg,
+    default_bg = gruvbox_colors.bg4,
+    colors = colors,
+    vi_mode_colors = {
+        NORMAL = "cyan",
+        OP = "cyan",
+        INSERT = "white",
+        VISUAL = "yellow",
+        BLOCK = "green",
+        REPLACE = "yellow",
+        ["V-REPLACE"] = "yellow",
+        ENTER = "cyan",
+        MORE = "cyan",
+        SELECT = "magenta",
+        COMMAND = "orange",
+        SHELL = "skyblue",
+        TERM = "skyblue",
+        NONE = "orange",
     },
-    mid = {
-      active = {
-        {
-          provider = "lsp_client_names",
-          hl = { fg = "fg", bg = gruvbox_colors.bg4 },
-          left_sep = { str = "", hl = { fg = gruvbox_colors.bg4, bg = gruvbox_colors.bg2 } },
-          right_sep = { str = "", hl = { fg = gruvbox_colors.bg4, bg = gruvbox_colors.bg2 } },
-          enabled = conditions.hide_in_width,
+    components = {
+        left = {
+            active = {
+                {
+                    provider = vi_mode_provider,
+                    hl = vi_mode_hl,
+                },
+                -- { provider = "", hl = vi_mode_hl },
+                {
+                    provider = "file_info",
+                    left_sep = " ",
+                    right_sep = { str = "", hl = { fg = "black", bg = gruvbox_colors.bg4 } },
+                },
+                {
+                    provider = "position",
+                    hl = { fg = "violet", bg = "black" },
+                },
+                { provider = "", hl = { fg = "black", bg = "bg" } },
+                {
+                    provider = "git_branch",
+                    icon = "  ",
+                    right_sep = " ",
+                    enabled = conditions.get_git_diff,
+                },
+                { provider = "git_diff_added", hl = { fg = "green" } },
+                { provider = "git_diff_changed", hl = { fg = "yellow" } },
+                { provider = "git_diff_removed", hl = { fg = "red" } },
+                { provider = "", hl = { fg = "bg", bg = "black" } },
+            },
+            inactive = {
+                { provider = vi_mode_provider, hl = vi_mode_hl, right_sep = " " },
+                {
+                    provider = "git_branch",
+                    icon = " ",
+                    right_sep = "  ",
+                    enabled = function()
+                        return vim.b.gitsigns_status_dict ~= nil
+                    end,
+                },
+                { provider = "file_info" },
+                { provider = "", hl = { fg = "bg", bg = "black" } },
+            },
         },
-      },
-      inactive = {},
+        mid = {
+            active = {
+                {
+                    provider = "lsp_client_names",
+                    hl = { fg = "fg", bg = gruvbox_colors.bg4 },
+                    left_sep = { str = "", hl = { fg = gruvbox_colors.bg4, bg = gruvbox_colors.bg2 } },
+                    right_sep = { str = "", hl = { fg = gruvbox_colors.bg4, bg = gruvbox_colors.bg2 } },
+                    enabled = conditions.hide_in_width,
+                },
+            },
+            inactive = {},
+        },
+        right = {
+            active = {
+                {
+                    provider = function()
+                        return get_diag("Error")
+                    end,
+                    hl = { fg = "bg", bg = "red", style = "bold" },
+                    left_sep = { str = " ", hl = { fg = "red", bg = gruvbox_colors.bg2 } },
+                    right_sep = { str = "", hl = { fg = "yellow", bg = "red" } },
+                },
+                {
+                    provider = function()
+                        return get_diag("Warning")
+                    end,
+                    hl = { fg = "bg", bg = "yellow", style = "bold" },
+                    right_sep = { str = "", hl = { fg = "cyan", bg = "yellow" } },
+                },
+                {
+                    provider = function()
+                        return get_diag("Information")
+                    end,
+                    hl = { fg = "bg", bg = "cyan", style = "bold" },
+                    right_sep = { str = "", hl = { fg = "oceanblue", bg = "cyan" } },
+                },
+                {
+                    provider = function()
+                        return get_diag("Hint")
+                    end,
+                    hl = { fg = "bg", bg = "oceanblue", style = "bold" },
+                    right_sep = { str = "", hl = { fg = "bg", bg = "oceanblue" } },
+                },
+                { provider = "file_type", left_sep = " ", enabled = conditions.hide_in_width },
+                {
+                    provider = "file_encoding",
+                    left_sep = " ",
+                    enabled = conditions.hide_in_width,
+                },
+                {
+                    provider = file_osinfo,
+                    left_sep = " ",
+                    hl = {
+                        style = "bold",
+                    },
+                    enabled = conditions.hide_in_width,
+                },
+                { provider = percentage_provider, hl = vi_mode_hl },
+            },
+            inactive = {},
+        },
     },
-    right = {
-      active = {
-        {
-          provider = function()
-            return get_diag("Error")
-          end,
-          hl = { fg = "bg", bg = "red", style = "bold" },
-          left_sep = { str = " ", hl = { fg = "red", bg = gruvbox_colors.bg2 } },
-          right_sep = { str = "", hl = { fg = "yellow", bg = "red" } },
+    properties = {
+        force_inactive = {
+            filetypes = {
+                "NvimTree",
+                "packer",
+                "dap-repl",
+                "dapui_scopes",
+                "dapui_stacks",
+                "dapui_watches",
+                "dapui_repl",
+                "LspTrouble",
+                "dashboard",
+            },
+            buftypes = { "terminal", "dashboard" },
+            bufnames = {},
         },
-        {
-          provider = function()
-            return get_diag("Warning")
-          end,
-          hl = { fg = "bg", bg = "yellow", style = "bold" },
-          right_sep = { str = "", hl = { fg = "cyan", bg = "yellow" } },
-        },
-        {
-          provider = function()
-            return get_diag("Information")
-          end,
-          hl = { fg = "bg", bg = "cyan", style = "bold" },
-          right_sep = { str = "", hl = { fg = "oceanblue", bg = "cyan" } },
-        },
-        {
-          provider = function()
-            return get_diag("Hint")
-          end,
-          hl = { fg = "bg", bg = "oceanblue", style = "bold" },
-          right_sep = { str = "", hl = { fg = "bg", bg = "oceanblue" } },
-        },
-        { provider = "file_type", left_sep = " ", enabled = conditions.hide_in_width },
-        {
-          provider = "file_encoding",
-          left_sep = " ",
-          enabled = conditions.hide_in_width,
-        },
-        {
-          provider = file_osinfo,
-          left_sep = " ",
-          hl = {
-            style = "bold",
-          },
-          enabled = conditions.hide_in_width,
-        },
-        { provider = percentage_provider, hl = vi_mode_hl },
-      },
-      inactive = {},
     },
-  },
-  properties = {
-    force_inactive = {
-      filetypes = {
-        "NvimTree",
-        "packer",
-        "dap-repl",
-        "dapui_scopes",
-        "dapui_stacks",
-        "dapui_watches",
-        "dapui_repl",
-        "LspTrouble",
-        "dashboard",
-      },
-      buftypes = { "terminal", "dashboard" },
-      bufnames = {},
-    },
-  },
 })
