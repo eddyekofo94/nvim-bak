e7f4c33bb5f585b76e81731a55a52b68a087899b
Eddy Ekofo <eddyecko@gmail.com>
2021-10-05 16:50:19 +0100

    Update NvimTree

--- lua/eekofo/plugins/nvimtree.lua
+++ lua/eekofo/plugins/nvimtree.lua
@@ -1,14 +1,45 @@
-vim.g.nvim_tree_disable_netrw = 0 -- "1 by default, disables netrw
--- vim.g.nvim_tree_hijack_netrw = 0 --"1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
-vim.g.nvim_tree_hide_dotfiles = 1 -- 0 by default, this option hides files and folders starting with a dot `.`
-vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent markers when folders are open
-vim.g.nvim_tree_follow = 1 -- "0 by default, this option allows the cursor to be updated when entering a buffer
-vim.g.nvim_tree_auto_close = O.auto_close_tree -- 0 by default, closes the tree when it's the last window
-vim.g.nvim_tree_auto_ignore_ft = { "dashboard", "startify" } --empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_ignore = { ".git", "node_modules", ".cache" } --empty by default
+vim.g.nvim_tree_gitignore = 1 --0 by default
+vim.g.nvim_tree_quit_on_open = 0 --0 by default, closes the tree when you open a file
+vim.g.nvim_tree_indent_markers = 1 --0 by default, this option shows indent markers when folders are open
+vim.g.nvim_tree_hide_dotfiles = 1 --0 by default, this option hides files and folders starting with a dot `.`
+vim.g.nvim_tree_git_hl = 1 --0 by default, will enable file highlight for git attributes (can be used without the icons).
+vim.g.nvim_tree_highlight_opened_files = 1 --0 by default, will enable folder and file icon highlight for opened files/directories.
+vim.g.nvim_tree_root_folder_modifier = ":~" --This is the default. See :help filename-modifiers for more options
+vim.g.nvim_tree_add_trailing = 1 --0 by default, append a trailing slash to folder names
+vim.g.nvim_tree_group_empty = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
+vim.g.nvim_tree_disable_window_picker = 0 --0 by default, will disable the window picker.
+vim.g.nvim_tree_icon_padding = " " --one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
+vim.g.nvim_tree_symlink_arrow = " >> " -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
+vim.g.nvim_tree_respect_buf_cwd = 1 --0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
+vim.g.nvim_tree_create_in_closed_folder = 0 --1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
+vim.g.nvim_tree_refresh_wait = 500 --1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
+vim.g.nvim_tree_window_picker_exclude = {
+    filetype = {
+        "notify",
+        "packer",
+        "qf",
+    },
+    buftype = {
+        "terminal",
+    },
+}
+-- Dictionary of buffer option names mapped to a list of option values that
+-- indicates to the window picker that the buffer's window should not be
+-- selectable.
+-- vim.g.nvim_tree_special_files = { 'README.md'= 1, 'Makefile'= 1, 'MAKEFILE'= 1 } -- List of filenames that gets highlighted with NvimTreeSpecialFile
+vim.g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 1,
+}
+
+-- a list of groups can be found at `:help nvim_tree_highlight`
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
-vim.g.nvim_tree_bindings = {
-    -- default mappings
+local list = {
+   -- default mappings
     { key = { "<CR>", "o", "l" }, cb = tree_cb("edit") },
     { key = "h", cb = tree_cb("close_node") },
     { key = "R", cb = tree_cb("refresh") },
@@ -20,25 +51,68 @@ vim.g.nvim_tree_bindings = {
     { key = "c", cb = tree_cb("copy") },
     { key = "p", cb = tree_cb("paste") },
 }
-
-vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
-vim.g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    -- git = {
-    --     unstaged = "?",
-    --     staged = "?",
-    --     unmerged = "?",
-    --     renamed = "?",
-    --     untracked = "?",
-    --     deleted = "?",
-    --     ignored = "?",
-    -- },
-    folder = {
-        default = "",
-        open = "",
-        empty = "",
-        empty_open = "",
-        symlink = "",
+-- following options are the default
+require("nvim-tree").setup({
+    -- disables netrw completely
+    disable_netrw = true,
+    -- hijack netrw window on startup
+    hijack_netrw = true,
+    -- open the tree when running this setup function
+    open_on_setup = false,
+    -- will not open on setup if the filetype is in this list
+    ignore_ft_on_setup = {},
+    -- closes neovim automatically when the tree is the last **WINDOW** in the view
+    auto_close = false,
+    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+    open_on_tab = false,
+    -- hijacks new directory buffers when they are opened.
+    update_to_buf_dir = {
+        -- enable the feature
+        enable = true,
+        -- allow to open the tree if it was previously closed
+        auto_open = true,
     },
-}
+    -- hijack the cursor in the tree to put it at the start of the filename
+    hijack_cursor = true,
+    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+    update_cwd = false,
+    -- show lsp diagnostics in the signcolumn
+    lsp_diagnostics = false,
+    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+    update_focused_file = {
+        -- enables the feature
+        enable = true,
+        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+        -- only relevant when `update_focused_file.enable` is true
+        update_cwd = false,
+        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+        ignore_list = {},
+    },
+    -- configuration options for the system open command (`s` in the tree by default)
+    system_open = {
+        -- the command to run this, leaving nil should work in most cases
+        cmd = nil,
+        -- the command arguments as a list
+        args = {},
+    },
+
+    view = {
+        -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
+        width = 30,
+        -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+        height = 30,
+        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+        side = "left",
+        -- if true the tree will resize itself after opening a file
+        auto_resize = true,
+        mappings = {
+            -- custom only false will merge the list with the default mappings
+            -- if true, it will only use your list to set the mappings
+            custom_only = false,
+            -- list of mappings to set on the tree manually
+            list = list ,
+        },
+    },
+})
+
