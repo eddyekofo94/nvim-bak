116f8e712d7123f624e65a9068edbe3e7dd28ffc
Eddy Ekofo <eddyecko@gmail.com>
2021-11-06 22:55:16 +0000

    packer plugins early

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 require('impatient')
 require('packer_compiled')
 vim.cmd("luafile ~/.config/nvim/lua/eekofo/globals/init.lua")
+vim.cmd("luafile ~/.config/nvim/lua/eekofo/install-plugins.lua")
 vim.cmd("luafile ~/.config/nvim/globals.lua")
 vim.cmd("luafile ~/.config/nvim/base-settings.lua")
 require("eekofo.globals")
