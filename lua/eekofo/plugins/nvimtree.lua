8ad2f868fd663b862c15fc1382fdf645ca36e35f
Eddy Ekofo <eddyecko@gmail.com>
2022-05-26 12:26:26 +0100

    Fix the nvim-tree to nvim 7

--- lua/eekofo/plugins/nvimtree.lua
+++ lua/eekofo/plugins/nvimtree.lua
@@ -1,27 +1,12 @@
-vim.g.nvim_tree_gitignore = 1 --0 by default
-vim.g.nvim_tree_quit_on_open = 0 --0 by default, closes the tree when you open a file
-vim.g.nvim_tree_indent_markers = 1 --0 by default, this option shows indent markers when folders are open
-vim.g.nvim_tree_git_hl = 1 --0 by default, will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_highlight_opened_files = 1 --0 by default, will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_root_folder_modifier = ":~" --This is the default. See :help filename-modifiers for more options
 vim.g.nvim_tree_add_trailing = 0 --0 by default, append a trailing slash to folder names
 vim.g.nvim_tree_group_empty = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
-vim.g.nvim_tree_disable_window_picker = 0 --0 by default, will disable the window picker.
 vim.g.nvim_tree_icon_padding = " " --one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
 vim.g.nvim_tree_symlink_arrow = " >> " -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
 vim.g.nvim_tree_respect_buf_cwd = 1 --0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 vim.g.nvim_tree_create_in_closed_folder = 0 --1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
 vim.g.nvim_tree_refresh_wait = 500 --1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
-vim.g.nvim_tree_window_picker_exclude = {
-    filetype = {
-        "notify",
-        "packer",
-        "qf",
-    },
-    buftype = {
-        "terminal",
-    },
-}
 -- Dictionary of buffer option names mapped to a list of option values that
 -- indicates to the window picker that the buffer's window should not be
 -- selectable.
@@ -36,6 +21,21 @@ vim.g.nvim_tree_show_icons = {
 -- a list of groups can be found at `:help nvim_tree_highlight`
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
+local renderer = {
+    indent_markers = {
+        enable = true,
+        icons = {
+            corner = "└ ",
+            edge = "│ ",
+            none = "  ",
+        },
+    },
+    icons = {
+        webdev_colors = true,
+        git_placement = "before",
+    },
+}
+
 local list = {
     -- default mappings
     { key = { "<CR>", "o", "l" }, cb = tree_cb("edit") },
@@ -49,8 +49,11 @@ local list = {
     { key = "c", cb = tree_cb("copy") },
     { key = "p", cb = tree_cb("paste") },
 }
+
 -- following options are the default
 require("nvim-tree").setup({
+    -- INFO: see if this doesn't slow down the machine
+    auto_reload_on_write = true,
     -- disables netrw completely
     disable_netrw = true,
     -- hijack netrw window on startup
@@ -59,10 +62,9 @@ require("nvim-tree").setup({
     open_on_setup = false,
     -- will not open on setup if the filetype is in this list
     ignore_ft_on_setup = {},
-    -- closes neovim automatically when the tree is the last **WINDOW** in the view
-    auto_close = false,
     -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
     open_on_tab = false,
+    sort_by = "name",
     -- hijacks new directory buffers when they are opened.
     diagnostics = {
         enable = true,
@@ -76,6 +78,31 @@ require("nvim-tree").setup({
     filters = {
         dotfiles = false,
         custom = { ".git" },
+    },
+    git = {
+        enable = true,
+        ignore = true,
+        timeout = 400,
+    },
+    actions = {
+        use_system_clipboard = true,
+        change_dir = {
+            enable = true,
+            global = false,
+            restrict_above_cwd = false,
+        },
+        open_file = {
+            quit_on_open = false,
+            resize_window = true,
+            window_picker = {
+                enable = true,
+                chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+                exclude = {
+                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+                    buftype = { "nofile", "terminal", "help" },
+                },
+            },
+        },
     },
     -- hijack the cursor in the tree to put it at the start of the filename
     hijack_cursor = true,
@@ -99,6 +126,7 @@ require("nvim-tree").setup({
         -- the command arguments as a list
         args = {},
     },
+    renderer = renderer,
     view = {
         -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
         width = 30,
