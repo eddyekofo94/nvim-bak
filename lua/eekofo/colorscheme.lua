21fbc2ee6a016a3c2cf7116ef66f87137d2fa9e0
Eddy Ekofo <eddy.ekofo@external.equans.com>
2023-02-15 14:58:47 +0100

    Update telescope theme

--- lua/eekofo/colorscheme.lua
+++ lua/eekofo/colorscheme.lua
@@ -1,16 +1,63 @@
 vim.cmd("set background=dark")
+-- local colors = require("catppuccin.palettes.mocha")
+local editor = require("catppuccin.groups.editor")
 
-vim.cmd.colorscheme "catppuccin"
-require("catppuccin").setup {
-      flavour = "mocha", -- mocha, macchiato, frappe, latte
-      term_colors = true,
-      integrations = {
-        nvimtree = true,
+require("catppuccin").setup({
+    flavour = "mocha", -- mocha, macchiato, frappe, latte
+    term_colors = true,
+    highlight_overrides = {
+        all = function(colors)
+            local normal = editor.get().Normal.fg
+            local fg, bg = normal.fg, normal.bg
+            local bg_alt = editor.get().Visual.bg
+            local green = colors.green
+            local red = colors.red
+            return {
+                TelescopeBorder = { fg = bg_alt, bg = bg },
+                TelescopeNormal = { bg = bg },
+                TelescopePreviewBorder = { fg = bg, bg = bg },
+                TelescopePreviewNormal = { bg = bg },
+                TelescopePreviewTitle = { fg = bg, bg = green },
+                TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
+                TelescopePromptNormal = { fg = fg, bg = bg_alt },
+                TelescopePromptPrefix = { fg = red, bg = bg_alt },
+                TelescopePromptTitle = { fg = bg, bg = red },
+                TelescopeResultsBorder = { fg = bg, bg = bg },
+                TelescopeResultsNormal = { bg = bg },
+                TelescopeResultsTitle = { fg = bg, bg = bg },
+            }
+        end,
+    },
+    integrations = {
+        fidget = true,
         cmp = true,
         gitsigns = true,
+        neotree = true,
         telescope = true,
-        treesitter = true
-      }
-    }
-local colors = require('catppuccin.palettes.mocha')
-vim.cmd.highlight { 'Tabline', 'guifg=' .. colors.green, 'guibg=' .. colors.mantle }
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+        navic = {
+            enabled = true,
+            custom_bg = "NONE",
+        },
+        native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = { "italic" },
+                hints = { "italic" },
+                warnings = { "italic" },
+                information = { "italic" },
+            },
+            underlines = {
+                errors = { "underline" },
+                hints = { "underline" },
+                warnings = { "underline" },
+                information = { "underline" },
+            },
+        },
+    },
+})
+local colors = require("catppuccin.palettes.mocha")
+vim.cmd.highlight({ "Tabline", "guifg=" .. colors.green, "guibg=" .. colors.mantle })
+vim.cmd.colorscheme("catppuccin")
