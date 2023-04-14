1253e14578830d3c2434bd6e28566c82bd117cb1
Eddy Ekofo <ekofoeddy@gmail.com>
2023-04-14 15:06:32 +0200

    Refactored comments on the whole file

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,20 +3,20 @@ return {
         "neovim/nvim-lspconfig",
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
-            { "neovim/nvim-lspconfig" }, -- Required
-            { "williamboman/mason.nvim" }, -- Optional
+            { "neovim/nvim-lspconfig" },             -- Required
+            { "williamboman/mason.nvim" },           -- Optional
             { "williamboman/mason-lspconfig.nvim" }, -- Optional
             {
                 "hrsh7th/nvim-cmp",
                 config = function()
                     require("plugins.lsp.completion")
                 end,
-            }, -- Required
-            { "hrsh7th/cmp-nvim-lsp" }, -- Required
-            { "hrsh7th/cmp-buffer" }, -- Optional
-            { "hrsh7th/cmp-path" }, -- Optional
+            },                              -- Required
+            { "hrsh7th/cmp-nvim-lsp" },     -- Required
+            { "hrsh7th/cmp-buffer" },       -- Optional
+            { "hrsh7th/cmp-path" },         -- Optional
             { "saadparwaiz1/cmp_luasnip" }, -- Optional
-            { "hrsh7th/cmp-nvim-lua" }, -- Optional
+            { "hrsh7th/cmp-nvim-lua" },     -- Optional
             { "hrsh7th/cmp-cmdline" },
             { "onsails/lspkind-nvim" },
             -- Snippets
@@ -32,17 +32,10 @@ return {
                     history = true,
                     delete_check_events = "TextChanged",
                 },
-            }, -- Required
+            },                                  -- Required
             { "rafamadriz/friendly-snippets" }, -- Optional
-            { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+            { "folke/neoconf.nvim",          cmd = "Neoconf", config = true },
             "hrsh7th/cmp-nvim-lsp",
-            -- {
-            --     "folke/neodev.nvim",
-            --     opts = {
-            --         plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
-            --         pathStrict = true,
-            --     },
-            -- },
             "simrat39/rust-tools.nvim",
             {
                 "j-hui/fidget.nvim",
@@ -70,20 +63,20 @@ return {
                         mode = "document_diagnostics",
                         action_keys = {
                             -- key mappings for actions in the trouble list
-                            close = "q", -- close the list
-                            cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-                            refresh = "r", -- manually refresh
-                            jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
-                            jump_close = { "o" }, -- jump to the diagnostic and close the list
-                            toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
-                            toggle_preview = "P", -- toggle auto_preview
-                            hover = "K", -- opens a small poup with the full multiline message
-                            preview = "p", -- preview the diagnostic location
+                            close = "q",                  -- close the list
+                            cancel = "<esc>",             -- cancel the preview and get back to your last window / buffer / cursor
+                            refresh = "r",                -- manually refresh
+                            jump = { "<cr>", "<tab>" },   -- jump to the diagnostic or open / close folds
+                            jump_close = { "o" },         -- jump to the diagnostic and close the list
+                            toggle_mode = "m",            -- toggle between "workspace" and "document" diagnostics mode
+                            toggle_preview = "P",         -- toggle auto_preview
+                            hover = "K",                  -- opens a small poup with the full multiline message
+                            preview = "p",                -- preview the diagnostic location
                             close_folds = { "zM", "zm" }, -- close all folds
-                            open_folds = { "zR", "zr" }, -- open all folds
+                            open_folds = { "zR", "zr" },  -- open all folds
                             toggle_fold = { "zA", "za" }, -- toggle fold of current file
-                            previous = "k", -- preview item
-                            next = "j", -- next item
+                            previous = "k",               -- preview item
+                            next = "j",                   -- next item
                         },
                     })
                 end,
