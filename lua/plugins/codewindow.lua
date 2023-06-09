6b0c613bf221a40354b8d1c06f303a6c130d4e82
Eddy Ekofo <ekofoeddy@gmail.com>
2023-06-09 16:18:15 +0200

    Added code window - minimap view

---
+++ lua/plugins/codewindow.lua
@@ -0,0 +1,9 @@
+return {
+    -- 'gorbit99/codewindow.nvim',
+    -- event = "BufEnter",
+    -- config = function()
+    --     local codewindow = require('codewindow')
+    --     codewindow.setup()
+    --     codewindow.apply_default_keybinds()
+    -- end,
+}
