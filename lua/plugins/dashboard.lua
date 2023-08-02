70e2c47b5ebd12401ce5f9c14c15e368ad5a45f8
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-08-02 09:38:34 +0000

    Change dashboard config

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,34 +1,59 @@
 return {
     "ChristianChiarulli/dashboard-nvim",
+    event = 'VimEnter',
     config = function()
-        vim.g.dashboard_default_executive = "telescope"
-
-        vim.g.dashboard_custom_header = {
-            "     ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-            "     ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-            "     ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-            "     ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-            "     ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-            "     ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+        require('dashboard').setup {
+            header = {
+                "     ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+                "     ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+                "     ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+                "     ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+                "     ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+                "     ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            }, --your header
+            change_to_vcs_root = true,
+            hide = {
+                "number",
+                "relativenumber",
+                "listchars",
+                "statusline", -- hide statusline default is true
+                "tabline",    -- hide the tabline
+                "winbar",     -- hide winbar
+            },
+            footer = {
+                "2 Timothy 2:15",
+            }, 
         }
-        vim.g.dashboard_custom_section = {
-            -- a = { description = { "  Load Last Session  " }, command = 'lua require("persistence").load()' },
-            a = { description = { "  Load Last Session  " }, command = 'SessionLoad' },
-            b = {
-                description = { "  Recently Used Files" },
-                command = "Telescope oldfiles",
-            },
-            c = {
-                description = { "  Find File          " },
-                command = "Telescope find_files",
-            },
-            d = {
-                description = { "  Find Word          " },
-                command = "Telescope live_grep",
-            },
-            -- e = {description = {'  Marks              '}, command = 'Telescope marks'}
-        }
-
-        vim.g.dashboard_custom_footer = { "2 Timothy 2:15" }
     end,
+    -- config = function()
+    --     vim.g.dashboard_default_executive = "telescope"
+
+    --     vim.g.dashboard_custom_header = {
+    --         "     ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    --         "     ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    --         "     ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    --         "     ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    --         "     ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    --         "     ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    --     }
+    --     vim.g.dashboard_custom_section = {
+    --         -- a = { description = { "  Load Last Session  " }, command = 'lua require("persistence").load()' },
+    --         a = { description = { "  Load Last Session  " }, command = 'SessionLoad' },
+    --         b = {
+    --             description = { "  Recently Used Files" },
+    --             command = "Telescope oldfiles",
+    --         },
+    --         c = {
+    --             description = { "  Find File          " },
+    --             command = "Telescope find_files",
+    --         },
+    --         d = {
+    --             description = { "  Find Word          " },
+    --             command = "Telescope live_grep",
+    --         },
+    --         -- e = {description = {'  Marks              '}, command = 'Telescope marks'}
+    --     }
+
+    --     vim.g.dashboard_custom_footer = { "2 Timothy 2:15" }
+    -- end,
 }
