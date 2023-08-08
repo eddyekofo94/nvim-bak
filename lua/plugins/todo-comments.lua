43ce29cb5a30bb80d5b480521937b883b694c024
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-08-08 07:56:44 +0000

    Added some todo configs

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -6,20 +6,20 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
         keywords = {
-            FIX   = {
+            FIX     = {
                 icon = ' ',                                       -- icon used for the sign, and in search results
                 color = 'error',                                     -- can be a hex color, or a named color (see below)
                 alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', 'ERROR' }, -- a set of other keywords that all map to this FIX keywords
                 -- signs = false, -- configure signs for some keywords individually
             },
-            TODO  = { icon = ' ', color = 'info' },
-            HACK  = { icon = '󰈸 ', color = 'warning' },
-            WARN  = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
-            PERF  = { icon = '󰅒 ', alt = { 'REFACTOR', 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
-            NOTE  = { icon = '󰆈 ', color = 'hint', alt = { 'INFO' } },
-            MAC   = { icon = '󰄛 ', color = 'hint', alt = { 'MAC' } },
-            DEBUG = { icon = ' ', color = 'error' },
-            TEST  = { icon = '󰙨 ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+            TODO    = { icon = ' ', color = 'info' },
+            HACK    = { icon = '󰈸 ', color = 'warning' },
+            WARN    = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+            PERF    = { icon = '󰅒 ', alt = { 'REFACTOR', 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+            NOTE    = { icon = '󰆈 ', color = 'hint', alt = { 'INFO' } },
+            EXAMPLE = { icon = '󰄛 ', color = 'hint', alt = { 'E.G.' } },
+            DEBUG   = { icon = ' ', color = 'error' },
+            TEST    = { icon = '󰙨 ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
         },
     },
     -- config = function()
