e04d67edbcd0882909455da56c0c985f4f7e622e
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-08-09 13:40:39 +0000

    change cmp sorting

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -70,6 +70,17 @@ return {
         })
 
         cmp.setup({
+            enabled = function()
+                -- disable completion in comments
+                local context = require 'cmp.config.context'
+                -- keep command mode completion enabled when cursor is in a comment
+                if vim.api.nvim_get_mode().mode == 'c' then
+                    return true
+                else
+                    return not context.in_treesitter_capture("comment")
+                        and not context.in_syntax_group("Comment")
+                end
+            end,
             snippet = {
                 expand = function(args)
                     require("luasnip").lsp_expand(args.body)
@@ -172,11 +183,6 @@ return {
             sorting = {
                 priority_weight = 1.0,
                 comparators = {
-                    compare.recently_used,
-                    compare.score,
-                    compare.length,
-                    compare.exact,
-                    compare.offset,
                     function(entry1, entry2) -- sort by length ignoring "=~"
                         local len1 = string.len(string.gsub(entry1.completion_item.label, "[=~()]",
                             ""))
@@ -200,7 +206,9 @@ return {
                             return t1 < t2
                         end
                     end,
-                    compare.scopes,
+                    compare.offset,
+                    compare.exact,
+                    compare.score,
                     compare.order,
                 },
             },
