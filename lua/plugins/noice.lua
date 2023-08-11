d7467a346ed46048dbb96141e269e4582c56dc5b
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-08-11 15:27:22 +0000

    Remove messages that keeps popping up! ANNOYING!!

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -16,18 +16,20 @@ return {
                 },
             },
             routes = {
-                -- notify when starting to record a macro
                 {
-                    view = "notify",
-                    filter = { event = "msg_showmode" },
-                },
+                    filter = {
+                        event = "msg_show",
+                        kind = "",
+                    },
+                    opts = { skip = true },
+                }, 
             },
             -- you can enable a preset for easier configuration
             presets = {
                 bottom_search = true,         -- use a classic bottom cmdline for search
                 command_palette = true,       -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false,            -- enables an input dialog for inc-rename.nvim
+                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = true,        -- add a border to hover docs and signature help
             },
             keys = {
