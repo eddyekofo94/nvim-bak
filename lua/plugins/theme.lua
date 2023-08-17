8fda0d9ff96451098264eb96c042f955d0d96153
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-08-17 07:58:45 +0000

    Add another theme which is nice and neat

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,16 @@
 return {
     {
+        "mcchrish/zenbones.nvim",
+        -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+        -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+        -- In Vim, compat mode is turned on as Lush only works in Neovim.
+        dependencies = "rktjmp/lush.nvim",
+        lazy = false,
+        config = function()
+            vim.cmd('colorscheme zenbones')
+        end,
+    },
+    {
         "rose-pine/neovim",
         config = function()
             require("rose-pine").setup({
