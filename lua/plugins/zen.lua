b44878b0c2cfdd5d33f2982e52453a5e1d005478
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-09-08 16:14:12 +0000

    Updated the zen mode

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -1,8 +1,8 @@
 return {
     {
         "folke/zen-mode.nvim",
-        event = "VeryLazy",
-        dependencies  = {
+        event        = "VeryLazy",
+        dependencies = {
             {
                 "folke/twilight.nvim",
                 config = function()
@@ -13,20 +13,39 @@ return {
                 end,
             },
         },
-        config = function()
+        config       = function()
             require("zen-mode").setup {
+                plugins = {
+                    wezterm = {
+                        enabled = true,
+                        -- can be either an absolute font size or the number of incremental steps
+                        font = "+10", -- (10% increase per step)
+                    },
+                },
                 window = {
                     backdrop = 1,
                     height = 0.9,
                     -- width = 140,
                     options = {
+                        concealcursor = 'n',
+                        conceallevel = 3,
+                        cursorcolumn = false,
+                        cursorline = false,
+                        foldcolumn = '0',
+                        list = false,
                         number = false,
                         relativenumber = false,
-                        signcolumn = "no",
-                        list = false,
-                        cursorline = false,
+                        signcolumn = 'no',
+                        statusline = ' ',
                     },
                 },
+                on_open = function()
+                    -- require("noice").disable()
+                end,
+                -- callback where you can add custom code when the Zen window closes
+                on_close = function()
+                    -- require("noice").enable()
+                end,
             }
 
             require("twilight").setup {
