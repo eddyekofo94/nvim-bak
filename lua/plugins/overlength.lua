481394a4d7bcb516276402be094fe12f8dd65586
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-09-12 13:34:01 +0000

    Overlength improved

--- lua/plugins/overlength.lua
+++ lua/plugins/overlength.lua
@@ -4,10 +4,10 @@ return {
     config = function()
         require('overlength').setup({
             bg                 = "#840000",
-            default_overlength = nil, -- INFO: seems to not work
+            default_overlength = 80, -- INFO: seems to not work
             disable_ft         = { "dashboard", "which-key", "lazygit", "term" },
         })
-        require('overlength').set_overlength({ "go", "lua" }, 120)
+        require('overlength').set_overlength({ "go", "lua", "vim" }, 120)
         require('overlength').set_overlength({ "cpp", "bash" }, 80)
         require('overlength').set_overlength({ "rust", "python" }, 100)
     end,
