9b95c31b306be44c5bfe442b4279b729a3f04651
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-09-18 15:39:30 +0000

    changed some keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,60 +4,64 @@
 vim.opt.fillchars = { diff = " " }
 
 return {
-    {
-        "TimUntersberger/neogit",
-        cmd = "Neogit",
-        keys = {
-            {
-                "<leader>gs", "<cmd>Neogit status<CR>", desc = "Git status",
-            },
-            "<leader>gC",
-            {
-                "<leader>gd", "<cmd>Neogit diff<CR>", desc = "Git diff",
-            },
-            { "<leader>gc", "<cmd>Neogit commit<CR>", desc = "Git commit" },
-            { "<leader>gp", "<cmd>Neogit pull<CR>",   desc = "Git pull" },
-            { "<leader>gP", "<cmd>Neogit push<CR>",   desc = "Git push" },
-            { "<leader>gr", "<cmd>Neogit rebase<CR>", desc = "Git rebase" },
-            { "<leader>gl", "<cmd>Neogit log<CR>",    desc = "Git log" },
+  {
+    "TimUntersberger/neogit",
+    cmd = "Neogit",
+    keys = {
+      {
+        "<leader>gs",
+        "<cmd>Neogit status<CR>",
+        desc = "Git status",
+      },
+      "<leader>gC",
+      {
+        "<leader>gd",
+        "<cmd>Neogit diff<CR>",
+        desc = "Git diff",
+      },
+      { "<leader>gcc", "<cmd>Neogit commit<CR>", desc = "Git commit" },
+      { "<leader>gp", "<cmd>Neogit pull<CR>", desc = "Git pull" },
+      { "<leader>gP", "<cmd>Neogit push<CR>", desc = "Git push" },
+      { "<leader>gr", "<cmd>Neogit rebase<CR>", desc = "Git rebase" },
+      { "<leader>gl", "<cmd>Neogit log<CR>", desc = "Git log" },
+    },
+    config = function()
+      local map = require("utils").map
+      local cmd, call = vim.cmd, vim.call
+
+      local neogit = require("neogit")
+      neogit.setup({
+        commit_popup = {
+          kind = "auto",
         },
-        config = function()
-            local map = require("utils").map
-            local cmd, call = vim.cmd, vim.call
+        signs = {
+          section = { "", "" },
+          item = { "", "" },
+        },
+        integrations = { diffview = true },
+        disable_builtin_notifications = true,
+        disable_commit_confirmation = true,
+      })
 
-            local neogit = require("neogit")
-            neogit.setup({
-                commit_popup = {
-                    kind = "auto",
-                },
-                signs = {
-                    section = { "", "" },
-                    item = { "", "" },
-                },
-                integrations = { diffview = true },
-                disable_builtin_notifications = true,
-                disable_commit_confirmation = true,
-            })
+      map("n", "<leader>gco", require("telescope.builtin").git_branches, "Git checkout")
+      map("n", "<leader>gss", function()
+        neogit.open({
+          cwd = vim.fn.expand("%:p:h"),
+          kind = "auto",
+        })
+      end, "Neogit status")
 
-            map("n", "<leader>gC", require("telescope.builtin").git_branches, "Git checkout")
-            map("n", "<leader>gs", function()
-                neogit.open({
-                    cwd = vim.fn.expand("%:p:h"),
-                    kind = "auto",
-                })
-            end, "Neogit status")
-
-            local group = vim.api.nvim_create_augroup('MyCustomNeogitEvents', { clear = true })
-            -- giving me an error
-            vim.api.nvim_create_autocmd('User', {
-                pattern = 'NeogitPushComplete',
-                group = group,
-                callback = require('neogit').close,
-            })
-        end,
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        event = "VeryLazy",
-    },
+      local group = vim.api.nvim_create_augroup("MyCustomNeogitEvents", { clear = true })
+      -- giving me an error
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "NeogitPushComplete",
+        group = group,
+        callback = require("neogit").close,
+      })
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    event = "VeryLazy",
+  },
 }
