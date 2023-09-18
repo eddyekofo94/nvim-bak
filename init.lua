3184c3cd8853efb01307a3afc055aa19c9e7455c
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-09-18 12:40:56 +0000

    Modified the init file

--- init.lua
+++ init.lua
@@ -2,24 +2,26 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.cmd("set background=dark")
+vim.cmd("set termguicolors")
 
 require("keymaps")
 require("globals")
 require("settings")
+require("autocommands")
 
-require('utils.hjkl_notifier')
+require("utils.hjkl_notifier")
 
 require("lazy").setup("plugins") -- INFO: this should be on the LAST LINE
