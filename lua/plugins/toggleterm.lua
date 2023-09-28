38e501565d801017a9434598a2315f1d1fd4fb2c
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-09-28 10:09:57 +0000

    Added some Lazygit config on toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -16,8 +16,42 @@ return {
     direction = "float",
     close_on_exit = true,
   },
+  config = function()
+    -- code
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "single",
+        width = 250,
+        height = 250,
+      },
+      -- function to run on opening the terminal
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
+      -- function to run on closing the terminal
+      on_close = function(term)
+        vim.cmd("startinsert!")
+      end,
+    })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+  end,
   cmd = "ToggleTerm",
   keys = {
+    {
+      "<leader>G",
+      function()
+        _lazygit_toggle()
+      end,
+      desc = "Lazygit",
+    },
     {
       "<leader>tt",
       "<cmd>ToggleTerm<cr>",
