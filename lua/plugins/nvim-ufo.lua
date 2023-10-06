1036ff381768123b555f1ff0c04bce64207ea639
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-10-06 16:43:19 +0000

    Improved ufo lsp config

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -62,7 +62,22 @@ return {
       table.insert(newVirtText, { suffix, "MoreMsg" })
       return newVirtText
     end
+
     opts["fold_virt_text_handler"] = handler
+    -- Tell the server the capability of foldingRange,
+    -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+    local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+    for _, ls in ipairs(language_servers) do
+      require("lspconfig")[ls].setup({
+        capabilities = capabilities,
+        -- you can add other fields for setting up lsp server in this table
+      })
+    end
     require("ufo").setup(opts)
     vim.keymap.set("n", "zR", require("ufo").openAllFolds)
     vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
