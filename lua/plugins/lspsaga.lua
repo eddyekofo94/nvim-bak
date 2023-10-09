9141e636100510820635dae7cce31a7db1ec9f61
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-10-09 12:56:49 +0000

    Ran formatter

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,38 +1,38 @@
 return {
-    "nvimdev/lspsaga.nvim",
-    event = "BufWinEnter",
-    keys = {
-        {
-            "K",
-            "<cmd>Lspsaga hover_doc<CR>",
-            desc = "Saga hover",
-        },
-        {
-            "]d",
-            "<cmd>Lspsaga diagnostic_jump_next<cr>",
-            desc = "diagnostic jump next",
-        },
-        {
-            "[d",
-            "<cmd>Lspsaga diagnostic_jump_prev<cr>",
-            desc = "diagnostic jump prev",
-        },
+  "nvimdev/lspsaga.nvim",
+  event = "BufWinEnter",
+  keys = {
+    {
+      "K",
+      "<cmd>Lspsaga hover_doc<CR>",
+      desc = "Saga hover",
     },
-    config = function()
-        require("lspsaga").setup({
-            code_action = {
-                enable = false
-            },
-            code_action_lightbulb = {
-                enable = false,
-            },
-            ui = {
-                kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
-            },
-        })
-        local mapper = require("utils").keymap_set
-        mapper("n", "dl", "<cmd>Lspsaga show_line_diagnostics<CR>", "Saga show line diagnostics")
-        mapper("n", "K", "<cmd>Lspsaga hover_doc<CR>", "Saga hover")
-    end,
-    dependencies = { { "nvim-tree/nvim-web-devicons" } },
+    {
+      "]d",
+      "<cmd>Lspsaga diagnostic_jump_next<cr>",
+      desc = "diagnostic jump next",
+    },
+    {
+      "[d",
+      "<cmd>Lspsaga diagnostic_jump_prev<cr>",
+      desc = "diagnostic jump prev",
+    },
+  },
+  config = function()
+    require("lspsaga").setup({
+      code_action = {
+        enable = false,
+      },
+      code_action_lightbulb = {
+        enable = false,
+      },
+      ui = {
+        kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
+      },
+    })
+    local mapper = require("utils").keymap_set
+    mapper("n", "dl", "<cmd>Lspsaga show_line_diagnostics<CR>", "Saga show line diagnostics")
+    mapper("n", "K", "<cmd>Lspsaga hover_doc<CR>", "Saga hover")
+  end,
+  dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
