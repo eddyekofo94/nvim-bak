943b1e55b9207f9f25cfb8462c37613364cd50b4
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-10-12 13:17:01 +0000

    Clean up

--- snippets/all.lua
+++ snippets/all.lua
@@ -1,8 +1,7 @@
 local ls = require("luasnip")
 local parse = ls.parser.parse_snippet
 local s, t, i, c, r, f, sn =
-	ls.snippet, ls.text_node, ls.insert_node, ls.choice_node, ls.restore_node, ls.function_node,
-	ls.snippet_node
+  ls.snippet, ls.text_node, ls.insert_node, ls.choice_node, ls.restore_node, ls.function_node, ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 local u = require("utils.luasnip")
 local today = u.today
@@ -13,37 +12,38 @@ local comment_close = u.comment_close
 local snippets = {}
 
 local todos = {
-	"fix",
-	"todo",
-	"info",
-	"hack",
-	"example",
-	"warn",
-	"clean_up",
-	"debug",
-	"perf",
-	"review",
-	"note",
-	"test",
-	"bug",
-	"refc",
-	"question",
+  "fix",
+  "todo",
+  "info",
+  "hack",
+  "example",
+  "warn",
+  "clean_up",
+  "debug",
+  "perf",
+  "review",
+  "note",
+  "test",
+  "bug",
+  "refc",
+  "question",
 }
+
 for _, todo in ipairs(todos) do
-	table.insert(
-		snippets,
-		s(
-			todo,
-			fmt([[
+  table.insert(
+    snippets,
+    s(
+      todo,
+      fmt([[
       {} ]] .. string.upper(todo) .. [[: {} - {}{}
       ]], {
-				f(comment_open),
-				f(today),
-				i(1),
-				f(comment_close),
-			})
-		)
-	)
+        f(comment_open),
+        f(today),
+        i(1),
+        f(comment_close),
+      })
+    )
+  )
 end
 
 return snippets
