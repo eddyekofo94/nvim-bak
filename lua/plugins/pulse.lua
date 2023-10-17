89236e8be348a90f634705fa5221887c20b0b595
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-10-17 15:05:04 +0000

    Clean up some pulse command

--- lua/plugins/pulse.lua
+++ lua/plugins/pulse.lua
@@ -10,15 +10,14 @@ return {
     event = "VeryLazy",
     -- enabled = false,
     keys = {
-      { "n", "<leader>wse", "<cmd>StandEnable<CR>", desc = "Enable the stand timer" },
+      { "<leader>wse", "<cmd>StandEnable<CR>", desc = "Enable the stand timer" },
       {
-        "n",
         "<leader>wsn",
         "<cmd>StandNow<CR>",
         desc = "Stand, restarting the timer",
       },
-      { "n", "<leader>wsd", "<cmd>StandDisable<CR>", desc = "Disable the stand timer" },
-      { "n", "<leader>wsw", "<cmd>StandWhen<CR>", desc = "Display time remaining until next stand" },
+      { "<leader>wsd", "<cmd>StandDisable<CR>", desc = "Disable the stand timer" },
+      { "<leader>wsw", "<cmd>StandWhen<CR>", desc = "Display time remaining until next stand" },
     },
     config = function()
       require("stand").setup({
