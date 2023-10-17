983716774fc7e0a063395dde2e9a973454dddd24
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-10-17 15:56:47 +0000

    Added back hjkl warning

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -12,28 +12,14 @@ return {
         view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
         opts = {}, -- global options for the cmdline. See section on views
         ---@type table<string, CmdlineFormat>
-        format = {
-          -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
-          -- view: (default is cmdline view)
-          -- opts: any options passed to the view
-          -- icon_hl_group: optional hl_group for the icon
-          -- title: set to anything or empty string to hide
-          cmdline = { pattern = "^:", icon = "", lang = "vim" },
-          search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-          search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-          filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-          lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
-          help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
-          input = {}, -- Used by input()
-          -- lua = false, -- to disable a format, set to `false`
-        },
       },
       messages = {
         -- NOTE: If you enable messages, then the cmdline is enabled automatically.
         -- This is a current Neovim limitation.
         enabled = true, -- enables the Noice messages UI
         view = "notify", -- default view for messages
-        view_error = "messages", -- view for errors
+        -- view_error = "messages", -- view for errors
+        view_error = "notify", -- view for errors
         view_warn = "notify", -- view for warnings
         view_history = "messages", -- view for :messages
         view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
