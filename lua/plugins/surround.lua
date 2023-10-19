cd5b9c6bcf6a6c3db6f0d40c1a60bd9f6cea95ba
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-10-19 10:04:14 +0000

    Formatted some files

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -2,81 +2,82 @@
 -- Surround --
 --------------
 return {
-    'kylechui/nvim-surround',
-    event = "VeryLazy",
-    keys = {
-        { 's', mode = { 'n', 'x', 'o' } },
-        { 'S', mode = 'x' },
-    },
-    init = function()
-        local map = require('utils').map
-        local augroup = 'Surround'
+  "kylechui/nvim-surround",
+  event = "VeryLazy",
+  keys = {
+    { "s", mode = { "n", "x", "o" } },
+    { "S", mode = "x" },
+  },
+  init = function()
+    local map = require("utils").map
+    local augroup = "Surround"
 
-        -- map('n', 'S', 's$', { remap = true, desc = 'Surround until end of line' })
+    -- map('n', 'S', 's$', { remap = true, desc = 'Surround until end of line' })
 
-        local function filetype_surround(filetype, surrounds)
-            vim.api.nvim_create_autocmd('FileType', {
-                pattern = filetype,
-                callback = function()
-                    require('nvim-surround').buffer_setup({
-                        surrounds = surrounds,
-                    })
-                end,
-                group = augroup,
-            })
-        end
+    local function filetype_surround(filetype, surrounds)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = filetype,
+        callback = function()
+          require("nvim-surround").buffer_setup({
+            surrounds = surrounds,
+          })
+        end,
+        group = augroup,
+      })
+    end
 
-        vim.api.nvim_create_augroup(augroup, {})
+    vim.api.nvim_create_augroup(augroup, {})
 
-        filetype_surround('lua', {
-            F = {
-                -- Anonymous function
-                add = function()
-                    return { { 'function() return ' }, { ' end' } }
-                end,
-            },
-        })
-        filetype_surround('markdown', {
-            c = {
-                -- Code block
-                add = function()
-                    return { { '```', '' }, { '', '```' } }
-                end,
-            },
-        })
-        filetype_surround({ 'rust', 'typescript' }, {
-            T = {
-                -- Type
-                add = function()
-                    return {
-                        { vim.fn.input({ prompt = 'Type name: ' }) .. '<' }, { '>' },
-                    }
-                end,
-            },
-        })
-        filetype_surround({ 'typescript' }, {
-            s = {
-                -- String interpolation
-                add = function()
-                    return { { '${' }, { '}' } }
-                end,
-            },
-        })
-    end,
-    config = function()
-        require('nvim-surround').setup({
-            move_cursor = false,
-            keymaps = {
-                normal = 's',
-                normal_cur = 'ss',
-                visual = 's',
-                visual_line = 'S',
-            },
-            aliases = {
-                q = "\"",
-                Q = '\'',
-                A = '`',
-            },
-        })
-    end,
+    filetype_surround("lua", {
+      F = {
+        -- Anonymous function
+        add = function()
+          return { { "function() return " }, { " end" } }
+        end,
+      },
+    })
+    filetype_surround("markdown", {
+      c = {
+        -- Code block
+        add = function()
+          return { { "```", "" }, { "", "```" } }
+        end,
+      },
+    })
+    filetype_surround({ "rust", "typescript" }, {
+      T = {
+        -- Type
+        add = function()
+          return {
+            { vim.fn.input({ prompt = "Type name: " }) .. "<" },
+            { ">" },
+          }
+        end,
+      },
+    })
+    filetype_surround({ "typescript" }, {
+      s = {
+        -- String interpolation
+        add = function()
+          return { { "${" }, { "}" } }
+        end,
+      },
+    })
+  end,
+  config = function()
+    require("nvim-surround").setup({
+      move_cursor = false,
+      keymaps = {
+        normal = "s",
+        normal_cur = "ss",
+        visual = "s",
+        visual_line = "S",
+      },
+      aliases = {
+        q = '"',
+        Q = "'",
+        A = "`",
+      },
+    })
+  end,
 }
