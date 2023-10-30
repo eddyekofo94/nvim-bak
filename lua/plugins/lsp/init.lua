d5d05bc0ec25cc444387a7efe25169566ec637f1
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-10-30 09:55:30 +0000

    Clean up

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -55,7 +55,6 @@ return {
         "hinell/lsp-timeout.nvim",
         dependencies = { "neovim/nvim-lspconfig" },
         init = function()
-          -- vim.g["lsp-timeout-config"] = {
           vim.g.lspTimeoutConfig = {
             stopTimeout = 1000 * 60 * 5, -- ms, timeout before stopping all LSP servers
             startTimeout = 1000 * 10, -- ms, timeout before restart
