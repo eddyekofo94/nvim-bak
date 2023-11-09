e8e37a9e1beaf6fed7a403b4eccbd572df5f7076
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-11-09 16:09:39 +0000

    Improved formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -2,119 +2,56 @@ return {
   "stevearc/conform.nvim",
   event = "VeryLazy",
   config = function()
-    -- code
     require("conform").setup({
-      format_on_save = {
-        lsp_fallback = true,
-        timeout_ms = 500,
-      },
-      format_after_save = {
-        lsp_fallback = true,
-      },
-      log_level = vim.log.levels.ERROR,
-      notify_on_error = true,
+      -- Map of filetype to formatters
       formatters_by_ft = {
         lua = { "stylua" },
-        -- Conform will use the first available formatter in the list
-        javascript = { "prettier_d", "prettier" },
-        -- Formatters can also be specified with additional options
-        python = {
-          formatters = { "autopep8", "isort", "black" },
-          -- Run formatters one after another instead of stopping at the first success
-          run_all_formatters = true,
-        },
-        go = {
-          formatters = { "gofumpt", "goimports_reviser", "gofmt", "golines" },
-          run_all_formatters = true,
-        },
-        json = {
-          formatters = { "jq" },
-        },
+        -- Conform will run multiple formatters sequentially
+        go = { "gofumpt", "goimports_reviser", "gofmt", "golines" }, -- "goimports", "gofmt"
+        json = { "jq" },
+
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { "prettierd", "prettier" } },
+        -- You can use a function here to determine the formatters dynamically
+        python = function(bufnr)
+          if require("conform").get_formatter_info("ruff_format", bufnr).available then
+            return { "ruff_format" }
+          else
+            return { "autopep8", "isort", "black" }
+          end
+        end,
         sh = {
           "beautysh",
         },
-        ["*"] = { "trim_whitespace" },
+        -- Use the "*" filetype to run formatters on all filetypes.
+        -- ["*"] = { "codespell" },
+        -- Use the "_" filetype to run formatters on filetypes that don't
+        -- have other formatters configured.
+        ["_"] = { "trim_whitespace" },
       },
+      -- If this is set, Conform will run the formatter on save.
+      -- It will pass the table to conform.format().
+      -- This can also be a function that returns the table.
+      format_on_save = {
+        -- I recommend these options. See :help conform.format for details.
+        lsp_fallback = false,
+        timeout_ms = 500,
+      },
+      -- If this is set, Conform will run the formatter asynchronously after save.
+      -- It will pass the table to conform.format().
+      -- This can also be a function that returns the table.
+      format_after_save = {
+        lsp_fallback = false,
+      },
+      -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+      log_level = vim.log.levels.ERROR,
+      -- Conform will notify you when a formatter errors
+      notify_on_error = true,
+      -- Custom formatters and changes to built-in formatters
+      formatters = {},
     })
   end,
   keys = {
     { "<leader>cf", '<cmd>lua require("conform").format()<cr>', desc = "Format current file" },
   },
 }
--- return {
---     "mhartington/formatter.nvim",
---     event = "VeryLazy",
---     --cmd = "Format",
---     config = function()
---         -- Utilities for creating configurations
---         local util = require("formatter.util")
---
---         -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
---         require("formatter").setup({
---             format_on_save = {
---                 lsp_fallback = true,
---                 timeout_ms = 500,
---             },
---             format_after_save = {
---                 lsp_fallback = true,
---             },
---             notify_on_error = true,
---             -- Enable or disable logging
---             logging = true,
---             -- Set the log level
---             log_level = vim.log.levels.WARN,
---             -- All formatter configurations are opt-in
---             filetype = {
---                 -- Formatter configurations for filetype "lua" go here
---                 -- and will be executed in order
---                 lua = {
---                     -- "formatter.filetypes.lua" defines default configurations for the
---                     -- "lua" filetype
---                     require("formatter.filetypes.lua").stylua,
---
---                     -- You can also define your own configuration
---                     function()
---                         -- Supports conditional formatting
---                         if util.get_current_buffer_file_name() == "special.lua" then
---                             return nil
---                         end
---
---                         -- Full specification of configurations is down below and in Vim help
---                         -- files
---                         return {
---                             exe = "stylua",
---                             args = {
---                                 "--search-parent-directories",
---                                 "--stdin-filepath",
---                                 util.escape_path(util.get_current_buffer_file_path()),
---                                 "--",
---                                 "-",
---                             },
---                             stdin = true,
---                         }
---                     end,
---                 },
---                 go = {
---                     formatters = { 'gofumpt', 'goimports', 'golines' },
---                     run_all_formatters = true,
---                 },
---                 json = {
---                     formatters = { 'jq' },
---                 },
---                 python = {
---                     require("formatter.filetypes.python").autopep8,
---                 },
---                 sh = {
---                     require("formatter.filetypes.sh").shfmt,
---                 },
---                 -- Use the special "*" filetype for defining formatter configurations on
---                 -- any filetype
---                 ["*"] = {
---                     -- "formatter.filetypes.any" defines default configurations for any
---                     -- filetype
---                     require("formatter.filetypes.any").remove_trailing_whitespace,
---                 },
---             },
---         })
---     end,
--- }
