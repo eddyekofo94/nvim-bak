f7e859d96934e7ebec498f4f0f665db6975de8a6
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-11-21 15:50:31 +0000

    Fix spider w to not skip punctuation

--- lua/plugins/spider.lua
+++ lua/plugins/spider.lua
@@ -4,11 +4,13 @@ local nxo = require("utils").nxo
 return {
   "chrisgrieser/nvim-spider",
   opts = {
-    skipInsignificantPunctuation = false,
+    skipInsignificantPunctuation = true,
   },
   keys = { "w", "e", "b", "ge" },
   config = function()
-    set(nxo, "w", "<cmd>lua require('spider').motion('w')<CR>", "Spider-w")
+    set(nxo, "w", function()
+      require("spider").motion("w", { skipInsignificantPunctuation = false })
+    end, "Spider-w")
     set(nxo, "e", "<cmd>lua require('spider').motion('e')<CR>", "Spider-e")
     set(nxo, "b", "<cmd>lua require('spider').motion('b')<CR>", "Spider-b")
     set(nxo, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
